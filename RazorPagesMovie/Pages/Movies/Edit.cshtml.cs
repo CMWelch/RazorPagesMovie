89efcd8c1496d2f938be37acbd955341b0e75e73
Connor Welch <kudude@att.net>
2022-08-05 16:03:31 -0500

    Simplify type names and rename titles and link names

--- RazorPagesMovie/Pages/Movies/Edit.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Edit.cshtml.cs
@@ -13,9 +13,9 @@ namespace RazorPagesMovie.Pages.Movies
 {
     public class EditModel : PageModel
     {
-        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+        private readonly RazorPagesMovieContext _context;
 
-        public EditModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
+        public EditModel(RazorPagesMovieContext context)
         {
             _context = context;
         }
